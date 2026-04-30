af327fad72d1a3bebbd21ee2c0fd45363ac5470c
Farheen <syedfarheen37@gmail.com>
2026-04-30 14:00:43 +0530

    fix route error

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// fallback route
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
